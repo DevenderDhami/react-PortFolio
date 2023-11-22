848b24b34208409f7e467876f231790b6f11ed6c
Devender Dhami <devender.dhami.99@gmail.com>
2023-11-22 23:04:09 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Nav from './Components/Navbar';
 import Footer from "./Components/Footer";
@@ -7,54 +7,55 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Project from "./Components/Projects";
 import MoveToTop from "./Components/MoveToTop";
-import Lottie from  "lottie-react";
+import Lottie from "lottie-react";
 import nightsky from "./LottieFiles/night-sky.json";
 import HashLoader from "react-spinners/HashLoader";
 
 
 function App() {
-  const[Loading,SetLoading]=useState(true);
+  const [Loading, SetLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     SetLoading(true)
 
-    setTimeout(()=>{
-    SetLoading(false)}
-    ,2000)
-  },[])  
-  
+    setTimeout(() => {
+      SetLoading(false)
+    }
+      , 2000)
+  }, [])
+
   return (
     <>
       {Loading ? (
-      <div className="loader"> 
-        <HashLoader
-          color={'#9067C6'}
-          loading={true}
-          size={100}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-      ):(
-      <div>
-      
-      <Lottie className="bg" animationData={nightsky} loop={true} />  
-      <Lottie className="bgtwo" animationData={nightsky} loop={true} />   
-      <Lottie className="bgtemp" animationData={nightsky} loop={true} /> 
+        <div className="loader">
+          <HashLoader
+            color={'#9067C6'}
+            loading={true}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
 
-      <Nav/>
-      <MoveToTop/>
+          <Lottie className="bg" animationData={nightsky} loop={true} />
+          <Lottie className="bgtwo" animationData={nightsky} loop={true} />
+          <Lottie className="bgtemp" animationData={nightsky} loop={true} />
 
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Project" element={<Project/>}/>
-        <Route path="/Resume" element={<Resume/>}/>
-      </Routes>
-      
-      <Footer/>
-      </div>
-      )}  
+          <Nav />
+          <MoveToTop />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Project" element={<Project />} />
+              <Route path="/Resume" element={<Resume />} />
+              <Route path="*" element={<Resume />} />
+            </Routes>
+
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
---
+++ src/Components/Error404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div>
+      error page not found
+    </div>
+  )
+}
+
+export default Error404
