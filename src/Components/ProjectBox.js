90aea6bb9f85034a8444f10d88fb342ea5e8d62f
Devender Dhami <devender.dhami.99@gmail.com>
2023-12-16 15:15:31 +0530

    projects added

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,21 +5,15 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
-
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
-
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
     
     "Medpreneur WebDesc":"Medpreneur is a single page static Website. Medpreneur is made using HTML5, CSS, and JavaScript .",
-    "Medpreneur WebGithub":"",
     "Medpreneur WebWebsite":"https://www.medpreneur.in/",
+
+    HexaTpDesc : "Hexatp is made using NextJs and Tailwind CSS ,It is fully functional website made for client - HexaBull",
+    HexaTpWebsite : "https://caselaws.luezoid.com/",
+
+    AgeCalculatorDesc : "AgeCalculator is made using React. This website is build to calculate the age on the  current date.",
+    AgeCalculatorWebsite : "https://devenderdhami.github.io/react-ageCalculator/",
   }
 
   let show ='';
@@ -37,9 +31,6 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
             {desc[projectName + 'Desc']}
             <br />
 
-            <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank' rel='noreferrer'>
-              <button className='projectbtn'><FaGithub/> Github</button>
-            </a>
 
             <a href={desc[projectName + 'Website']} target='_blank' rel='noreferrer'>
               <button className='projectbtn'><CgFileDocument/>Go to Website</button>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ProjectBox from '../Components/ProjectBox';
 import MedpreneurWeb from '../images/medreneur-web.png';
+import HexaTp from '../images/HexaTp.png';
+import AgeCalculator from '../images/Age-Calculator.png';
 import Layout from '../Components/AppLayout/Layout';
 
 const Projects = () => {
@@ -9,6 +11,8 @@ const Projects = () => {
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
         <ProjectBox projectPhoto={MedpreneurWeb} projectName="Medpreneur Web" />
+        <ProjectBox projectPhoto={HexaTp} projectName="HexaTp" />
+        <ProjectBox projectPhoto={AgeCalculator} projectName="AgeCalculator" />
         {/* <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" /> */}
         {/* <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" /> */}
         {/* <ProjectBox projectPhoto={TindogImage} projectName="Tindog" /> */}
